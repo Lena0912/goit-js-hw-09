3cd96c38799a17b5089c25064fba912d657005d5
Lena0912 <elenabelichenko9@gmail.com>
2023-08-21 23:52:34 +0300

    first task correct

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -16,6 +16,7 @@ function changeBodyColor() {
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+let timerId;
 
 function clickStart() {
     elements.stop.disabled = false;
